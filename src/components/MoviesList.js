7306e0d87cae5fbdbdb31560c909d66eb4421589
Frank Faustino <nawzyah@gmail.com>
2017-08-23 17:03:27 -0700

    Completed MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,10 +1,28 @@
 // Import React from react.
 
 // Create a const arrow function called MoviesList.
-  // Return an unordered list
-    // Inside the unordered list, embed a javascript expression (curly brackets).
-      // Inside the javascript expression, map the movies array
-        // The function inside map should take in two arguments, movie and i (for index).
-        // The function should return a list item, with a javascript expression movie (the same variable as map).
-        // The list item should have an attribute called key, that takes in i as a javascript expression.
+// Return an unordered list
+// Inside the unordered list, embed a javascript expression (curly brackets).
+// Inside the javascript expression, map the movies array
+// The function inside map should take in two arguments, movie and i (for index).
+// The function should return a list item, with a javascript expression movie (the same variable as map).
+// The list item should have an attribute called key, that takes in i as a javascript expression.
 // Outside the arrow function, export the function MoviesList as a default.
+
+import React from "react";
+
+const MoviesList = (props) => {
+  return (
+    <ul>
+      {props.map((movie, i) => {
+        return (
+          <li key={i}>
+            {movie}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default MoviesList;
