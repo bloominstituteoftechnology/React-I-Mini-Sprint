47ec3162f7961a1e5748f8cd5d39a88f5e63fa14
Frank Faustino <nawzyah@gmail.com>
2017-08-23 17:34:45 -0700

    Completed MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -14,13 +14,13 @@ import React from "react";
 const MoviesList = (props) => {
   return (
     <ul>
-      {props.map((movie, i) => {
+      { props.movies.map((movie, i) => {
         return (
           <li key={i}>
             {movie}
           </li>
         );
-      })}
+      }) }
     </ul>
   );
 };
