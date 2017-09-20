511e4c10777bb89e24214b9271193cb05a86b4fb
sara fritz <krumsara@gmail.com>
2017-09-20 18:59:10 -0500

    mini sprint

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 // Step 1: Import React, and Component from react.
 // Step 2: Import render from react-dom
 // Step 3: Import MoviesList from MoviesList
-
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import MoviesList from './MoviesList.js';
 // Step 4: Write a class called App, extending Component.
   // Step 5: Write a constructor function without passing anything into it.
     // Step 6: Inside the constructor function, call super without passing anything into it.
@@ -12,3 +14,19 @@
       // Step 11: Inside the return statement, write a selfclosing tag called MoviesList
         // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket. 
 // Step 13: Outside the class, export the App class as a default. 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: ['Mad Max: Fury Road', 'Birdman', 'Super Troopers']
+    };
+  }
+
+  render() {
+    return (
+      <MoviesList movies={this.state.movies} />
+    );
+  }
+}
+
+export default App
