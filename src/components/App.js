0a75c3564410943da36c2a36189f0ebcc3510a2e
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2017-09-21 15:14:27 -0400

    Mini-Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
 // Step 1: Import React, and Component from react.
+import React from "react";
+import { Component } from "react";
 // Step 2: Import render from react-dom
+import { render } from "react-dom";
 // Step 3: Import MoviesList from MoviesList
+import MoviesList from "./MoviesList.js";
 
 // Step 4: Write a class called App, extending Component.
+class App extends Component {
   // Step 5: Write a constructor function without passing anything into it.
+  constructor() {
     // Step 6: Inside the constructor function, call super without passing anything into it.
+    super();
     // Step 7: Inside the constructor function, create a state on this, and set it to an object.
+    this.state = {
       // Step 8: Inside the object, set a key to movies, and the value to an array of movies
+      movies: ["movie1", "movie2", "movie3", "movie4", "movie5"]
+    };
+  }
   // Step 9: Write a render function.
+  render() {
     // Step 10: Inside the render function, write a return statement.
+    return (
       // Step 11: Inside the return statement, write a selfclosing tag called MoviesList
-        // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket. 
-// Step 13: Outside the class, export the App class as a default. 
+      // Step 12: Inside the selfclosing tag, give it an attribute called movies,
+      //and pass it this.state.movies inside a set of curly bracket.
+      
+      <MoviesList movies={this.state.movies} />
+    );
+  }
+}
+// Step 13: Outside the class, export the App class as a default.
+export default App;
