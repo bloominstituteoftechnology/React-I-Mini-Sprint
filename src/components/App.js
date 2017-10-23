9d70d060771d3f7b827fb3f943fbf584e5aad2e7
Ryan Hamblin <ryanhca@gmail.com>
2017-10-23 14:19:22 -0600

    even more ammo on solush

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,30 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        'Forrest Gump',
-        'Blade Runner',
-        'Super Bad',
-        'Enemy of the State',
-        'The Matrix'
-      ]
+      movieTitle: '',
+      movies: []
     };
+    this.handleMovieTitle = this.handleMovieTitle.bind(this);
+    this.handleAddMovieToList = this.handleAddMovieToList.bind(this);
+  }
+  handleMovieTitle(event) {
+    this.setState({ movieTitle: event.target.value });
+  }
+  handleAddMovieToList() {
+    let title = this.state.movieTitle;
+    const movies = this.state.movies;
+    movies.push(title);
+    this.setState({ movies, movieTitle: '' });
   }
   render() {
-    return <MoviesList movies={this.state.movies} />;
+    return (
+      <MoviesList
+        changeHandler={this.handleMovieTitle}
+        addToMovies={this.handleAddMovieToList}
+        title={this.state.movieTitle}
+        movies={this.state.movies}
+      />
+    );
   }
 }
 
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const MoviesList = props => {
   return (
-    <ul>
-      {props.movies.map((movie, i) => {
-        return <li key={i + 1}>{movie}</li>;
-      })}
-    </ul>
+    <div>
+      <input type="text" value={props.title} onChange={props.changeHandler} />
+      <button onClick={props.addToMovies}>Submit Movie</button>
+      <ul>
+        {props.movies.map((movie, i) => {
+          return <li key={i + 1}>{movie}</li>;
+        })}
+      </ul>
+    </div>
   );
 };
 
