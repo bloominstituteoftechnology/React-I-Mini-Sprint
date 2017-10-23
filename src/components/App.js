67e40d25f8b8730dab62840f142ea98ff6c175a5
Christian Franco <christian.franco94@icloud.com>
2017-10-23 12:08:08 -0700

    Completed minilab

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 // Step 1: Import React, and Component from react.
-// Step 2: Import render from react-dom
+import React, { Component } from 'react';
 // Step 3: Import MoviesList from MoviesList
-
+import MoviesList from './MoviesList';
 // Step 4: Write a class called App, extending Component.
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                'Forest Gump',
+                'Blade Runner',
+                'Super Bad',
+                'The Matrix',
+                'Enemy of the State'
+            ]
+        };
+    }
+    render() {
+        return (
+            <div>
+                <MoviesList movies={this.state.movies}/>
+            </div>
+        )
+    }
+}
 // Step 5: Write a constructor function without passing anything into it.
 // Step 6: Inside the constructor function, call super without passing anything into it.
 // Step 7: Inside the constructor function, create a state on this, and set it to an object.
@@ -12,3 +33,5 @@
 // Step 11: Inside the return statement, write a selfclosing tag called MoviesList
 // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
 // Step 13: Outside the class, export the App class as a default.
+
+export default App;
