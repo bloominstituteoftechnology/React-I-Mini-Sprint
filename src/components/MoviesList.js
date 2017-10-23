22ad5bb4363e9074154c27aacd295522e462ab18
Rashmi Baheti <baheti.rash123@gmail.com>
2017-10-23 14:16:02 -0700

    completed React mini project

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,16 +2,16 @@
 import React from 'react';
 
 const MoviesList = props => {
-	return <div>Hello from within Movies List</div>
+  return (
+    <div>
+      <input type="text" value={props.title} onChange={props.changeHandler} />
+      <button onClick={props.addToMovies}>Submit Movie</button>
+      <ul>
+        {props.movies.map((movie, i) => {
+          return <li key={i + 1}>{movie}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
-
 export default MoviesList;
-
-// Create a const arrow function called MoviesList.
-// Return an unordered list
-// Inside the unordered list, embed a javascript expression (curly brackets).
-// Inside the javascript expression, map the movies array
-// The function inside map should take in two arguments, movie and i (for index).
-// The function should return a list item, with a javascript expression movie (the same variable as map).
-// The list item should have an attribute called key, that takes in i as a javascript expression.
-// Outside the arrow function, export the function MoviesList as a default.
