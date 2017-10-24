bf5e5f186a9c2bcd7328dc7227c224ecbbe12ed6
Ronald Willis II <32463687+oggiedoggy12@users.noreply.github.com>
2017-10-24 15:33:50 -0500

    Update MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,5 +1,21 @@
 // Import React from react.
+import React from 'react';
 
+const MoviesList = props => {
+    return (
+      <div>
+        <input type="text" value={props.title} onChange={props.changeHandler} />
+        <button onClick={props.addToMovies}>Submit Movie</button>
+        <ul>
+          {props.movies.map((movie, i) => {
+            return <li key={i + 1}>{movie}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  };
+
+export default MoviesList;
 // Create a const arrow function called MoviesList.
 // Return an unordered list
 // Inside the unordered list, embed a javascript expression (curly brackets).
