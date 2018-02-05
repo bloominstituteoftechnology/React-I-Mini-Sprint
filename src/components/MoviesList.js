a26f7e2051faf6df4ba226a7bc704da3ffff32ef
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-05 14:49:38 -0700

    Add 'key' attribute to list item that takes in 'i' as a js expression

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -9,11 +9,11 @@ const MoviesList = () => {
     {movies.map( (movie, i) => { // Inside the javascript expression, map the movies array with two arguments, movie and i (for index).
       // The function should return a list item, with a javascript expression movie (the same variable as map).
       return (
-        <li>{movie}</li>
+        // The list item should have an attribute called key, that takes in i as a javascript expression.
+        <li key={i}>{movie}</li>
       );
     })}
     </ul>
   );
 };
-// The list item should have an attribute called key, that takes in i as a javascript expression.
 // Outside the arrow function, export the function MoviesList as a default.
