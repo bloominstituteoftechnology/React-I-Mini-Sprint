3a774df38dfa826b26a4268131df824f58c6d12b
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-05 14:31:46 -0700

    Create a const arrow function called MoviesList

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,6 +1,9 @@
 // Import React from react.
 import React from 'react';
 // Create a const arrow function called MoviesList.
+const MoviesList = () => {
+
+};
 // Return an unordered list
 // Inside the unordered list, embed a javascript expression (curly brackets).
 // Inside the javascript expression, map the movies array
