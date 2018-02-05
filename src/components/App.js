24a5b0729b3cb4664459de7474fe1e31b8e8d5e2
Archaic Audio <ArchaicAudio@Archaic-Audio.local>
2018-02-05 17:40:42 -0500

    Added title; cleaned up some code

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,6 @@ import MoviesList from './MoviesList';
 // Step 10: Inside the render function, write a return statement.
 // Step 11: Inside the return statement, write a selfclosing tag called MoviesList
 // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
-
 class App extends Component {
   constructor() {
     super();
@@ -25,11 +24,13 @@ class App extends Component {
 
   render() {
     return (
-      <MoviesList movies={this.state.movies} />
+      <div className='MoviesList'>
+        <h1>Movies List</h1>
+        <MoviesList movies={this.state.movies} />
+      </div>
     );
   }
 }
 
 // Step 13: Outside the class, export the App class as a default.
-
 export default App;
