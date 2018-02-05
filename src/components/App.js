b575db9f2fb4bda6af6345c52178686464ba676b
Lo Saephan <teamsky916@gmail.com>
2018-02-05 12:25:34 -0800

    modified app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 // Step 1: Import React, and Component from react.
+import React from 'react';
 // Step 2: Import render from react-dom
+import ReactDOM from 'react-dom';
 // Step 3: Import MoviesList from MoviesList
+import MoviesList from './MoviesList';
 
 // Step 4: Write a class called App, extending Component.
+class App extends Component {
+	constructor(){
+		super();
+
+		this.state = {};
+
+		render(){
+			return(
+				<MovieList movies="{this.state.movies}"/>
+			);
+		}
+	}
+}
+
+Export default App;
+
 // Step 5: Write a constructor function without passing anything into it.
 // Step 6: Inside the constructor function, call super without passing anything into it.
 // Step 7: Inside the constructor function, create a state on this, and set it to an object.
