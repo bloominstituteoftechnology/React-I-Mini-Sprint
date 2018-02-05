3b0676e96be30954b8e76ff8baef7f249273c8a1
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-05 14:35:38 -0700

    Inside the unordered list, embed a javascript expression

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,10 +4,9 @@ import React from 'react';
 const MoviesList = () => {
   // Return an unordered list
   return (
-    <ul></ul> 
+    <ul>{}</ul> // Inside the unordered list, embed a javascript expression (curly brackets).
   );
 };
-// Inside the unordered list, embed a javascript expression (curly brackets).
 // Inside the javascript expression, map the movies array
 // The function inside map should take in two arguments, movie and i (for index).
 // The function should return a list item, with a javascript expression movie (the same variable as map).
