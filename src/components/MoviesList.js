3f60575406458054168981fb24ccb9193d91e1ef
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-05 14:37:41 -0700

    Inside the javascript expression, map the movies array

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -2,12 +2,12 @@
 import React from 'react';
 // Create a const arrow function called MoviesList.
 const MoviesList = () => {
-  // Return an unordered list
+  // Return an unordered list.
   return (
-    <ul>{}</ul> // Inside the unordered list, embed a javascript expression (curly brackets).
+    // Inside the unordered list, embed a javascript expression (curly brackets).
+    <ul>{movies.map()}</ul> // Inside the javascript expression, map the movies array.
   );
 };
-// Inside the javascript expression, map the movies array
 // The function inside map should take in two arguments, movie and i (for index).
 // The function should return a list item, with a javascript expression movie (the same variable as map).
 // The list item should have an attribute called key, that takes in i as a javascript expression.
