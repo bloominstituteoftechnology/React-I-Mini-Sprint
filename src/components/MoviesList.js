be0bd48cc8e9907f6378c54fd4ad8e92f48c5072
Archaic Audio <ArchaicAudio@Archaic-Audio.local>
2018-02-05 16:39:30 -0500

    Compiling successfully but nothing being rendered

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -8,10 +8,10 @@ import React from 'react';
 // The function inside map should take in two arguments, movie and i (for index).
 // The function should return a list item, with a javascript expression movie (the same variable as map).
 // The list item should have an attribute called key, that takes in i as a javascript expression.
-const MoviesList = () => {
+MoviesList = () => {
   return (
     <ul>
-      {movies.map((movie, i) => {
+      {this.state.movies.map((movie, i) => {
         return (
           <li key={i}>{movie}</li>
         )
