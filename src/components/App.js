383fc7a542c2db08adae5dc8487548ef45560e86
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-05 14:19:13 -0700

    // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket

--- src/components/App.js
+++ src/components/App.js
@@ -19,9 +19,9 @@ class App extends Component {
     // Step 10: Inside the render function, write a return statement.
     return (
       // Step 11: Inside the return statement, write a selfclosing tag called MoviesList.
-      <MoviesList />
+      <MoviesList movies={this.state.movies}/> // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
     );
   }
 }
-// Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
+
 // Step 13: Outside the class, export the App class as a default.
