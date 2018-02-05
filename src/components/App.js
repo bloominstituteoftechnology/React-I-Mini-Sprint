4a45dd96d63bf506ce7954dfae7ec3a181aa79f8
Tyson Williams <tyson.j.williams@gmail.com>
2018-02-05 14:17:16 -0700

    Inside the return statement, write a selfclosing tag called MoviesList

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 // Step 1: Import React, and Component from react.
 import React from 'react';
 import Component from 'react';
-// Step 2: Import render from react-dom
+// Step 2: Import render from react-dom.
 import { render } from 'react-dom';
-// Step 3: Import MoviesList from MoviesList
+// Step 3: Import MoviesList from MoviesList.
 import MoviesList from './MoviesList';
 // Step 4: Write a class called App, extending Component.
 class App extends Component {
@@ -18,10 +18,10 @@ class App extends Component {
   render() {
     // Step 10: Inside the render function, write a return statement.
     return (
-
+      // Step 11: Inside the return statement, write a selfclosing tag called MoviesList.
+      <MoviesList />
     );
   }
 }
-// Step 11: Inside the return statement, write a selfclosing tag called MoviesList
 // Step 12: Inside the selfclosing tag, give it an attribute called movies, and pass it this.state.movies inside a set of curly bracket.
 // Step 13: Outside the class, export the App class as a default.
