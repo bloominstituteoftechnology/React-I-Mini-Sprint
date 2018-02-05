1e732983a0e79c54d8147d63b7b5cc4fe7fb4eee
Ronnie Miksch <fakepreferences@gmail.com>
2018-02-05 13:02:37 -0800

    Cannot read property createElement error

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,4 +1,5 @@
 // Import React from react.
+import { React } from "react";
 
 // Create a const arrow function called MoviesList.
 // Return an unordered list
@@ -8,3 +9,15 @@
 // The function should return a list item, with a javascript expression movie (the same variable as map).
 // The list item should have an attribute called key, that takes in i as a javascript expression.
 // Outside the arrow function, export the function MoviesList as a default.
+
+const MoviesList = (list) => {
+    return (
+    <ul>
+        {list.items.map((movie, i) => {
+            return <li key={i}>{movie}</li>
+        })};
+    </ul>
+    );
+};
+
+export default MoviesList;
