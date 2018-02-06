cb16fb66475ca4eb77a0da9b272258d1d09634ce
Charlie Sparks <csparks@antioch.edu>
2018-02-05 19:13:47 -0800

    not rendering but can't find any issues

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,6 +1,18 @@
 // Import React from react.
+import React from "react";
 
 // Create a const arrow function called MoviesList.
+const MoviesList = (props) => {
+  return (
+    <ul>
+      {props.movies.map((movie) => {
+        return <li>{movie}</li>
+      })}
+    </ul>
+    )
+}
+
+export default MoviesList;
 // Return an unordered list
 // Inside the unordered list, embed a javascript expression (curly brackets).
 // Inside the javascript expression, map the movies array
