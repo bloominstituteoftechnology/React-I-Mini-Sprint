cfa3386ecac6514fd37f7c2995d638ed6ca1b578
Charlie Sparks <csparks@antioch.edu>
2018-02-05 19:20:19 -0800

    fixed a few issues, still not rendering

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,22 +1,13 @@
-// Import React from react.
 import React from "react";
 
-// Create a const arrow function called MoviesList.
 const MoviesList = (props) => {
   return (
     <ul>
-      {props.movies.map((movie) => {
-        return <li>{movie}</li>
+      {props.movies.map((movie, i) => {
+        return <li key={i}>{movie}</li>
       })}
     </ul>
     )
 }
 
 export default MoviesList;
-// Return an unordered list
-// Inside the unordered list, embed a javascript expression (curly brackets).
-// Inside the javascript expression, map the movies array
-// The function inside map should take in two arguments, movie and i (for index).
-// The function should return a list item, with a javascript expression movie (the same variable as map).
-// The list item should have an attribute called key, that takes in i as a javascript expression.
-// Outside the arrow function, export the function MoviesList as a default.
