07bb9518454175567a73156ff82973964d61af00
jnandez0586 <jnandez0586@gmail.com>
2018-03-19 22:45:25 -0500

    Jorge Hernandez: Finished App.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,6 +1,12 @@
 // Import React from react.
+import React from "react";
 
 // Create a const arrow function called MoviesList.
+const MoviesList = () => {
+    return (
+        
+    )
+}
 // Return an unordered list
 // Inside the unordered list, embed a javascript expression (curly brackets).
 // Inside the javascript expression, map the movies array
